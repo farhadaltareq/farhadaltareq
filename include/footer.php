079cf858fbd661fd77dbac9fa6766e9925d61b4a
Abu Farhad Al Tareq <farhadaltareq@gmail.com>
2017-01-29 01:10:42 +0000

    First commit

---
+++ include/footer.php
@@ -0,0 +1,27 @@
+<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-10 col-lg-offset-1 text-center">
+                
+                
+               <!-- Footer starts down here -->     
+                        
+                <br>
+                    <ul class="list-inline">
+                        <li>
+                            <a href="https://www.facebook.com/tareq.mahmud2"><i class="fa fa-facebook fa-fw fa-3x"></i></a>
+                        </li>
+                        <li>
+                            <a href=""><i class="fa fa-twitter fa-fw fa-3x"></i></a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/farhadaltareq"><i class="fa fa-dribbble fa-fw fa-3x"></i></a>
+                        </li>
+                    </ul>
+                    <hr class="small">
+                    <p class="text-muted">Copyright &copy; Your Website 2014</p>
+                </div>
+            </div>
+        </div>
+        <a id="to-top" href="#top" class="btn btn-dark btn-lg"><i class="fa fa-chevron-up fa-fw fa-1x"></i></a>
+    </footer>
